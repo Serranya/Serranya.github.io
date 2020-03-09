8aecde6365bb9aeefc393e10af87300223167158
Peter Lamby <Peter.Lamby@direkt-gruppe.de>
2020-03-09 16:05:21 +0100

    Typo

--- articles/on_java_exceptions/WhoThrows.java
+++ articles/on_java_exceptions/WhoThrows.java
@@ -2,7 +2,7 @@ import static Example.method1;
 import static Example.method2;
 import static Example.method3;
 
-public class WhoTrhows {
+public class WhoThrows {
 	public static void main(String[] args) throws MyThrowable {
 		method1();
 		method2();
